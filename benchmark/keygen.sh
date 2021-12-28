7440147504d3ff52b84259c48de5c55f1a47e41b
K <kanishkatn@gmail.com>
2021-12-28 12:27:05 +0800

    change benchmark to 2 out of 2 scenario

--- benchmark/keygen.sh
+++ benchmark/keygen.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+#How to run
+# sh keysign.sh
+# copy the pubkey returned and use it in keysign.sh
+
 keygenRequest=$(cat <<EOF
               {
                   "keys":[
                       "thorpub1addwnpepqtdklw8tf3anjz7nn5fly3uvq2e67w2apn560s4smmrt9e3x52nt2svmmu3",
-                      "thorpub1addwnpepqtspqyy6gk22u37ztra4hq3hdakc0w0k60sfy849mlml2vrpfr0wvm6uz09",
-                      "thorpub1addwnpepq2ryyje5zr09lq7gqptjwnxqsy2vcdngvwd6z7yt5yjcnyj8c8cn559xe69",
-                      "thorpub1addwnpepqfjcw5l4ay5t00c32mmlky7qrppepxzdlkcwfs2fd5u73qrwna0vzag3y4j"
+                      "thorpub1addwnpepqtspqyy6gk22u37ztra4hq3hdakc0w0k60sfy849mlml2vrpfr0wvm6uz09"
                   ],
                   "tss_version":"0.14.0",
                   "block_height":100
@@ -28,14 +30,6 @@ start=`date +%s`
 {
   res2=$(do_keygen http://localhost:8081/keygen)
   echo "Response from Node 2 ${res2}"
-}&
-{
-  res3=$(do_keygen http://localhost:8082/keygen)
-  echo "Response from Node 3 ${res3}"
-}&
-{
-  res4=$(do_keygen http://localhost:8083/keygen)
-  echo "Response from Node 4 ${res4}"
 }&
 wait
 end=`date +%s`
--- benchmark/keysign.sh
+++ benchmark/keysign.sh
@@ -11,9 +11,7 @@ keysignRequest=$(cat <<EOF
                   "messages": ["$2"],
                   "signer_pub_keys": [
                       "thorpub1addwnpepqtdklw8tf3anjz7nn5fly3uvq2e67w2apn560s4smmrt9e3x52nt2svmmu3",
-                      "thorpub1addwnpepqtspqyy6gk22u37ztra4hq3hdakc0w0k60sfy849mlml2vrpfr0wvm6uz09",
-                      "thorpub1addwnpepq2ryyje5zr09lq7gqptjwnxqsy2vcdngvwd6z7yt5yjcnyj8c8cn559xe69",
-                      "thorpub1addwnpepqfjcw5l4ay5t00c32mmlky7qrppepxzdlkcwfs2fd5u73qrwna0vzag3y4j"
+                      "thorpub1addwnpepqtspqyy6gk22u37ztra4hq3hdakc0w0k60sfy849mlml2vrpfr0wvm6uz09"
                   ],
                   "tss_version":"0.14.0",
                   "block_height":100
@@ -36,14 +34,6 @@ start=`gdate +%s.%N`
 {
   res2=$(do_keysign http://localhost:8081/keysign)
   echo "Response from Node 2 ${res2}"
-}&
-{
-  res3=$(do_keysign http://localhost:8082/keysign)
-  echo "Response from Node 3 ${res3}"
-}&
-{
-  res4=$(do_keysign http://localhost:8083/keysign)
-  echo "Response from Node 4 ${res4}"
 }&
 wait
 end=`gdate +%s.%N`
